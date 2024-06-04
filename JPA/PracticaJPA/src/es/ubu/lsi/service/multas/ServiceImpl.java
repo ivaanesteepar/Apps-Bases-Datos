355674dfe4d824482aeddfa6e3c7844e3440b143
Iván Estépar <102294523+ivaanesteepar@users.noreply.github.com>
2024-06-04 10:52:10 +0200

    Update ServiceImpl.java

--- JPA/PracticaJPA/src/es/ubu/lsi/service/multas/ServiceImpl.java
+++ JPA/PracticaJPA/src/es/ubu/lsi/service/multas/ServiceImpl.java
@@ -54,16 +54,16 @@ public class ServiceImpl extends PersistenceService implements Service {
 			
 			conductor.setPuntos(puntosRestantes);
 			
-		// Construir la consulta de inserción manualmente	        
-	        Query query = em.createNativeQuery("INSERT INTO Incidencia (fecha, nif, idtipo) VALUES (:fecha, :nif, :tipo)");
-	               
-	        query.setParameter("fecha", fecha);
-	        query.setParameter("nif", nif);
-	        query.setParameter("tipo", tipo);
-	        
-	        // Ejecutar la consulta de inserción
-	        query.executeUpdate();
-	        
+			// Construir la consulta de inserción manualmente	        
+		        Query query = em.createNativeQuery("INSERT INTO Incidencia (fecha, nif, idtipo) VALUES (:fecha, :nif, :tipo)");
+		               
+		        query.setParameter("fecha", fecha);
+		        query.setParameter("nif", nif);
+		        query.setParameter("tipo", tipo);
+		        
+		        // Ejecutar la consulta de inserción
+		        query.executeUpdate();
+		        
 			commitTransaction(em);
 			
 		} catch (Exception e) {
@@ -76,3 +76,61 @@ public class ServiceImpl extends PersistenceService implements Service {
 			em.close();
 		}
 	}
+
+	@Override
+	public void indultar(String nif) throws PersistenceException {
+		EntityManager em = this.createSession();
+		try {
+			beginTransaction(em);
+			
+			ConductorDAO conductorDAO = new ConductorDAO(em);
+			Conductor conductor = conductorDAO.findById(nif);
+			
+			if (conductor == null) {
+				throw new IncidentException(IncidentError.NOT_EXIST_DRIVER);
+			} 
+			
+			conductor.setPuntos(new BigDecimal("12"));
+			
+			// Elimina todas las incidencias del conductor utilizando una consulta de eliminación
+		        Query query = em.createQuery("DELETE FROM Incidencia WHERE conductor = :conductor");
+		        query.setParameter("conductor", conductor);
+		        query.executeUpdate();
+	
+		        commitTransaction(em);
+
+		} catch (Exception e) {
+			rollbackTransaction(em);
+			if (e instanceof IncidentException) {
+				throw e;
+			}
+			logger.error(e.getLocalizedMessage());
+		} finally {
+			em.close();
+		}
+	}
+
+	@Override
+	public List<Vehiculo> consultarVehiculos() throws PersistenceException {
+	    EntityManager em = this.createSession();
+	    try {
+	    	List<Vehiculo> vehiculos = em.createQuery("SELECT v FROM Vehiculo v JOIN FETCH v.conductores c JOIN FETCH c.incidencias", Vehiculo.class)
+	                                    .getResultList();
+	        for (Vehiculo vehiculo : vehiculos) {
+	            System.out.println(vehiculo.toString());
+	            for (Conductor conductor : vehiculo.getConductores()) {
+	                System.out.println("\t" + conductor.toString());
+	                for (Incidencia incidencia : conductor.getIncidencias()) {
+	                    System.out.println("\t\t" + incidencia.toString());
+	                }
+	            }
+	        }
+	        return vehiculos;
+	    } catch (Exception e) {
+	        logger.error("Error al consultar los vehículos", e);
+	        throw new PersistenceException("Error al consultar los vehículos: " + e.getMessage(), e);
+	    } finally {
+	        em.close();
+	    }
+	}
+}
